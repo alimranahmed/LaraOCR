8a0a3fca569ee405be4d43c4cdaa777b5b88af50
Henning Ritter <1150063+Henning256@users.noreply.github.com>
2021-11-14 14:11:24 +0100

    implemented optional paramer $lang for function scan

--- src/Services/Tesseract.php
+++ src/Services/Tesseract.php
@@ -8,15 +8,21 @@ class Tesseract extends OcrAbstract
 
     protected $command;
 
-    public function scan($imagePath) {
+    public function scan($imagePath, $lang = null) {
 
+        if ($lang === null) {
+            $lang = env('OCR_LANG');
+        }
         $this->setImagePath($imagePath);
         $shell = new Shell();
 
         $executable = config('lara_ocr.engines.tesseract.executable', 'tesseract');
 
-        $command = trim("$executable $imagePath stdout quiet");
+        $command = trim($executable.(($lang !== null) ? ' -l '.$lang : '').' '.$imagePath.' stdout quiet');
 
         return $shell->execute($command);
     }
+    
+    // ToDo: implement function listLangs(): array
+    // ToDo: implement function langSupported(string $lang): bool
 }
