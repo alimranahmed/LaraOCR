c0eccd0602d39a2a0a1d9e7103ee9ab42fdb6adb
henning <henning@weteef.de>
2021-11-20 18:39:13 +0100

    implemented functions languageSupported and supportedLanguages

--- src/Services/Tesseract.php
+++ src/Services/Tesseract.php
@@ -6,6 +6,28 @@ class Tesseract extends OcrAbstract
 {
     protected $command;
 
+    protected function getExecutable()
+    {
+        return config('lara_ocr.engines.tesseract.executable', 'tesseract');
+    }
+
+    public function supportedLanguages()
+    {
+        $shell = new Shell();
+
+        $command = trim($this->getExecutable().' --list-langs');
+
+        $langArray = explode("\n", $shell->execute($command));
+        array_shift($langArray);  // remove heading "List of available languages" from command result
+        
+        return $langArray;
+    }
+
+    public function languageSupported($lang)
+    {
+        return in_array($lang, $this->supportedLanguages());
+    }
+
     public function scan($imagePath, $lang = null)
     {
         if ($lang === null) {
@@ -14,14 +36,10 @@ class Tesseract extends OcrAbstract
         $this->setImagePath($imagePath);
         $shell = new Shell();
 
-        $executable = config('lara_ocr.engines.tesseract.executable', 'tesseract');
         $langParam = ($lang !== null) ? ' -l '.$lang : '';
 
-        $command = trim($executable.$langParam.' '.$imagePath.' stdout quiet');
+        $command = trim($this->getExecutable().$langParam.' '.$imagePath.' stdout quiet');
 
         return $shell->execute($command);
     }
-    
-    // ToDo: implement function listLangs(): array
-    // ToDo: implement function langSupported(string $lang): bool
 }
